4d2d3e754e7b502506bea4ff0cd8d12352b07fe0
Lukas Pieper <30661176+lukaspieper@users.noreply.github.com>
2022-01-17 13:09:24 +0100

    Migrate BuildConfig reader/creator

--- Build.cs
+++ Build.cs
@@ -1,21 +1,41 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using Components;
+using JetBrains.Annotations;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using Nuke.Components;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
-partial class Build : NukeBuild, IRebuild, IClean, ICopyStaticArtifacts
+partial class Build : NukeBuild, IRebuild, IClean, ICopyStaticArtifacts, IHazBuildConfig
 {
     public IReadOnlyCollection<Output> MsBuildOutput { get; set; }
-    
-    BuildConfig BuildConfig { get; set; }
-    
-    AbsolutePath XsltDirectory => (this as IHazBuildDirectory).BuildDirectory / "Xslt";
+    public BuildConfig BuildConfig { get; set; }
+    public AbsolutePath XsltDirectory => (this as IHazBuildDirectory).BuildDirectory / "Xslt";
 
-    public static int Main() => Execute<Build>(build => build.CompileAndAnalyze);
+    public static int Main()
+    {
+        return Execute<Build>(build => build.CompileAndAnalyze);
+    }
 
-    protected override void OnBuildCreated() => BuildConfig = GetDeserializedBuildConfigOrDefault();
+    protected override void OnBuildCreated()
+    {
+        BuildConfig = (this as IHazBuildConfig).GetDeserializedBuildConfigOrDefault();
+    }
+    
+    [UsedImplicitly]
+    public Target OpenReport => _ => _
+        .Executes(() =>
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = (this as IHazArtifacts).ArtifactsDirectory / "Analysis.html",
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+        });
 }
---
+++ Components/IHazBuildConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text.Json;
+using JetBrains.Annotations;
+using Nuke.Common;
+using Nuke.Common.IO;
+
+namespace Components;
+
+public interface IHazBuildConfig : INukeBuild
+{
+    AbsolutePath BuildConfigFile => RootDirectory / "BuildConfig.json";
+    
+    [UsedImplicitly]
+    Target CreateBuildConfig => _ => _
+        .Executes(() =>
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var jsonText = JsonSerializer.Serialize(new BuildConfig(), serializerOptions);
+            File.WriteAllText(BuildConfigFile, jsonText);
+        });
+
+    public BuildConfig GetDeserializedBuildConfigOrDefault()
+    {
+        if (!File.Exists(BuildConfigFile)) return new BuildConfig();
+        
+        var jsonText = File.ReadAllText(BuildConfigFile);
+        return JsonSerializer.Deserialize<BuildConfig>(jsonText);
+    }
+}
